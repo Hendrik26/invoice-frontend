78ba9860c2a9e4ae6359746613e0b80a21caf92d
Hendrik26 <hendrik.herdler@gmail.com>
2018-09-07 11:05:44 +0200

    False files deleted. /Component invoice-list created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoiceListComponent
   ],
   imports: [
     BrowserModule
