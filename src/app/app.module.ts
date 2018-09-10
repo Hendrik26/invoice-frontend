931427c3863593a48ec39a2398b3bcb49aa5bcd5
hendr_8nkbeie <hendrik.herdler@stud.hszg.de>
2018-09-11 00:23:44 +0200

    add item-detail files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 import { InvoiceDetailComponent } from './invoice-detail/invoice-detail.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InvoiceListComponent,
-    InvoiceDetailComponent
+    InvoiceDetailComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule
